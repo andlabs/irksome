4de0b55a92d49505843fbeb03ab4102fc48cb929
Pietro Gagliardi <pietro10@mac.com>
2015-01-25 04:15:55 -0500

    Added formatted strings.

---
+++ iface/formatted.go
@@ -0,0 +1,32 @@
+// 25 january 2015
+package iface
+
+// Formatted represents a formatted string.
+// See Style for more information.
+type Formatted struct {
+	Text		string
+	Styles	[]Style
+}
+
+// Style contains instructions on how to style text.
+// Each field in Formatted.Styles represents a starting point; a style is applied until the end of the string or a countering Style, whichever comes first.
+type Style struct {
+	Start		int		// in bytes
+	Add		StyleBits	// bits of styles to add
+	Remove	StyleBits	// bits of styles to remove (TODO what if bits are set in both?)
+	FGColor	int		// if Add & FGColor != 0, foreground color index
+	BGColor	int		// if Add & BGColor != 0, background color index
+}
+
+// StyleBits represents styles to add or remove.
+type StyleBits uint64
+const (
+	Bold StyleBits = 1 << iota
+	Italic
+	Underline
+	Strikethrough
+	Monospace
+	Reverse
+	FGColor
+	BGColor
+)
