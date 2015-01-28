33b265def1b2192a924ddac63b3aa4751da9ca22
Pietro Gagliardi <pietro10@mac.com>
2015-01-27 22:44:46 -0500

    Decided to issue requests immediately rather than return Messages. This will also make the semantics of Message cleaner.

--- iface/server.go
+++ iface/server.go
@@ -2,60 +2,58 @@
 package iface
 
 // TODO settle the overloading of definitions of 'message'
-// TODO really return Messages and not send them immediately?
 // TODO flooding/multiline messages (for slack) (see first TODO)
 // TODO state thread safety of entire package
 
 // Server represents a server.
 type Server interface {
-	// Send and Recv return channels that send and receive, respectively, Messages to/from the server.
-	// The values of these channels are guaranteed not to change during the lifetime of the Server object, and they shall not be closed during said lifetime.
-	Send() chan<- Message
-	Recv() <-chan Message
+	// C returns a channel that will receive Messages from the Server.
+	// The value returned by C is guaranteed not to change during the lifetime of the Server object, and it shall not be closed during said lifetime.
+	C() <-chan Message
 
 	// Connect establishes the connection to the Server.
-	// After Connect, Send and Recv will begin processing messages.
-	// A Message of type Connected will be received once the connection has been established.
-	// A Message of type Error will be received if the connection fails.
+	// After Connect, C will begin sending messages.
+	// A Message of type Connected will be sent once the connection has been established.
+	// A Message of type Error will be sent if the connection fails.
 	// Connect may send protocol-specific handshake messages as necessary; the Server object's actual type should define an interface for this (for instance, for establishing the initial username and identifying information about a user).
 	// (TODO what happens if already connected)
 	Connect()
 
-	// Raw returns a Message that, when sent, issues the given byte sequence directly to the server.
+	// Raw issues the given byte sequence directly to the server.
 	// TODO hostmasks
-	Raw(sequence []byte) Message
+	Raw(sequence []byte)
 
-	// Join returns a Message that, when sent, ask the server to join a given channel.
-	// If the Join succeeds, a Message of type Joined will be received; the value of Channel() on that Message should be used for all future accesses to that channel (until the channel is left or the server is disconnected).
-	Join(channel string, password string) Message
+	// Join asks the server to join a given channel.
+	// If the Join succeeds, a Message of type Joined will be snet; the value of Channel() on that Message should be used for all future accesses to that channel (until the channel is left or the server is disconnected).
+	Join(channel string, password string)
 
 	// Query is similar to Join, except that instead of joining a channel, it initiates a private conversation ("query") with a given user.
 	// Because some protocols (IRC, for instance) don't have a special state for "querying someone else", Query() requires a line to send to the user at the same time.
-	// On success, the received Message type will be QueryStarted instead of Joined, but the semantics are the same.
-	Query(nick string, message []byte) Message
+	// On success, the Message type will be QueryStarted instead of Joined, but the semantics are the same.
+	Query(nick string, message []byte)
 
 	// QueryDo is similar to Query, except the message is sent as an action; see Channel.Do().
-	QueryDo(nick string, message []byte) Message
+	QueryDo(nick string, message []byte)
 
-	// Quit returns a Message that, when sent, will lead to being disconnected from the server.
+	// Quit issues a request to be disconnected from the server.
 	// You may specify an optional reason.
-	Quit(reason []byte) Message
+	Quit(reason []byte)
 
 	// TODO Nick()?
 
-	// SetNick returns a Message that, when sent, requests that the server change your nickname to the given nick.
-	// If successful, a Message of type YourNickChanged will be received.
-	SetNick(newnick string) Message
+	// SetNick requests that the server change your nickname to the given nick.
+	// If successful, a Message of type YourNickChanged will be sent.
+	SetNick(newnick string)
 
 	// Format takes the given Formatted and returns the byte sequence that it is equivalent to in the given protocol.
 	// Formatting styles that are not supported are merely ignored.
 	Format(f Formatted) []byte
 
-	// Away returns a Message that, when sent, marks yourself as away.
-	Away(reason string) Message
+	// Away marks yourself as away.
+	Away(reason string)
 
-	// Back returns a Message that, when sent, marks yourself as no longer away.
-	Back() Message
+	// Back marks yourself as no longer away.
+	Back()
 
 	// TODO user mode translation
 }
