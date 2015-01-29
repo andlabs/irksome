65d9247582fa9432f7282fe51e86429a48547582
Pietro Gagliardi <pietro10@mac.com>
2015-01-28 20:16:07 -0500

    Started integrating the test server into the main program.

---
+++ servers.go
@@ -0,0 +1,44 @@
+// 28 january 2015
+package main
+
+import (
+	"github.com/andlabs/irksome/iface"
+	"github.com/andlabs/irksome/testsrv"
+)
+
+var servers []iface.Server
+var channels [][]iface.Channel
+var currentServer, currentChannel int
+
+const (
+	sTest = iota
+	sIRC
+	sSlack
+)
+
+func openServer(ty int, addr string, port int, tls bool) {
+	var s iface.Server
+
+	switch ty {
+	case sTest:
+		srv := testsrv.NewServer()
+		srv.SetInitNick("andlabs")
+		s = srv
+	default:
+		panicf("unrecognized server type %d", ty)
+	}
+	// TODO add server to UI here
+	servers = append(servers, s)
+	s.Connect()
+	go listen(s)
+}
+
+func sendMessage(what string) {
+	servers[0].Raw([]byte(what))
+}
+
+func listen(s iface.Server) {
+	for m := range s.C() {
+		println(string(m.Raw()))
+	}
+}
