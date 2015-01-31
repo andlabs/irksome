32a61502e014ed55bb45ed0f18d9e35568f9780f
Pietro Gagliardi <pietro10@mac.com>
2015-01-31 18:30:17 -0500

    More integration work. I think I'm going to need to redefine iface again...

--- channels.go
+++ channels.go
@@ -19,7 +19,9 @@ var chans = make([]*channel, 0, 50)
 var addChannel = make(chan *channel)
 var channelAdded = make(chan struct{})
 
+// TODO merge
 var parentServers = make(map[iface.Server]C.gint64)
+var channelIDs = make(map[iface.Channel]C.gint64)
 
 type sendMessageParams struct {
 	message		string
@@ -27,6 +29,7 @@ type sendMessageParams struct {
 }
 
 var sendMessage = make(chan *sendMessageParams)
+var recvMessage = make(chan iface.Message)
 
 func doChannels() {
 	for {
@@ -37,15 +40,25 @@ func doChannels() {
 			parent := C.gint64(-1)
 			if chans[n].channel != nil {
 				parent = parentServers[chans[n].server]
+				channelIDs[chans[n].channel] = n
 			} else {
 				parentServers[chans[n].server] = n
 			}
 			C.tellUI(C.mAddChannel, strToArg(cc.name), C.TRUE, n, parent)
 			<-channelAdded
-			// TODO start monitoring
 		case m := <-sendMessage:
 			// TODO
 			_ = m
+		case m := <-recvMessage:
+			// TODO current channels per server
+			n, ok := channelIDs[m.Channel()]
+			if !ok {
+				n = parentServers[m.Server()]
+			}
+			// TODO
+			_ = n
 		}
 	}
 }
+
+func listen(server 
